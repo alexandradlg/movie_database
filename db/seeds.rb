1e08d9cbc18d4aacb6f1256fdf6ec453eff01db3
Alexandra DLG <adelagueronniere@gmail.com>
2018-04-30 12:44:11 +0200

    movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.all.destroy
+
+Movie.create(title: "Into the Wild", release_year: 2007, director: "Sean Penn")
+Movie.create(title: "The Rocky Horror Picture Show", release_year: 1975, director: "Jim Sharman")
+Movie.create(title: "Okja", release_year: 2017, director: "Bong Joon-ho")
+Movie.create(title: "The Kids Are All Right", release_year: 2010 , director: "Lisa Cholodenko")
+Movie.create(title: "Lost in Translation", release_year: 2003, director: "Sofia Coppola")
